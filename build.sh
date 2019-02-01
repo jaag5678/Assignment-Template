3ed5a02582c65254372e5859663fd35b15caab88
Akshay GOPALAKRISHNAN <aaksha@ds2.CS.McGill.CA>
2019-02-01 09:48:01 -0500

    Test cases

--- build.sh
+++ build.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Build the compiler
-#
-# You MUST replace the following commands with the commands for building your compiler
-
-#make clean -C ./src
-#make -C ./src
-
 cd src
 bison -d -v Parser.y
 flex -o Scan.c Scanner_Update.l
